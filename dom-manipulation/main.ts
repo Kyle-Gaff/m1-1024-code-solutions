770b7841f80ae2e72c9b2a82d2563e89bc0cbade
CharLee-Kylee Gaff <charleesti810@gmail.com>
2025-01-15 04:22:09 +0000

    completed dom manipulation

---
+++ dom-manipulation/main.ts
@@ -0,0 +1,34 @@
+let numberOfClicks = 0;
+
+const $clickCount = document.querySelector('.click-count');
+
+function incClick(): void {
+  numberOfClicks++;
+  console.log('Number of clicks:', numberOfClicks);
+
+  if (!$clickCount) throw new Error('$click-count query failed');
+  $clickCount.textContent = 'Clicks:' + numberOfClicks;
+
+  if (!$hotButton) throw new Error('.hot-button query failed');
+  if (numberOfClicks <= 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (numberOfClicks <= 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (numberOfClicks <= 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (numberOfClicks <= 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (numberOfClicks <= 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+}
+
+const $hotButton = document.querySelector('.hot-button');
+
+if (!$hotButton) throw new Error('.hot-button query failed');
+
+$hotButton.addEventListener('click', incClick);
+
+if (!$clickCount) throw new Error('$click-count query failed');
